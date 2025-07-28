241636198243301e0f0c87c9b977e1c5f589ee14
ShodanMaster <akshaykrishnan922@gmail.com>
2025-07-28 23:51:24 +0530

    Working With Where

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,6 +12,10 @@ class ArticleController extends Controller
      */
     public function index()
     {
+        // Example queries to demonstrate various Eloquent methods
+
+        //Working with Numbers
+
         //dd(Article::count()); //Count returns the number of articles
         //dd(Article::where('is_published', true)->count()); //Count returns the number of published articles
         // dd(Article::pluck('user_id')->countBy()); //group by perticular column and return the count of group
@@ -24,6 +28,31 @@ class ArticleController extends Controller
         // dd(Article::inRandomOrder()->value('id')); //Get a random article id
         // dd(Article::sum('minutes_to_read')); //Get the sum of a column
 
+
+        //Working with
+
+        //Where
+        $collection = collect([
+            ['id' => 1, 'name' => 'Akshay', 'age' => 25],
+            ['id' => 2, 'name' => 'John', 'age' => 30],
+            ['id' => 3, 'name' => 'Jane', 'age' => 28],
+        ]);
+
+        // dd($collection->where('age', 25)); //Filter the collection where age is 25
+        // dd($collection->where('age', '>', 25)); //Filter the collection where age is greater than 25
+
+        // dd(Article::where('is_published', true)->get()); //Get all published articles
+        // dd(Article::where('is_published', true)->where('minutes_to_read', 7)->get()); //Get all published articles with 7 minutes to read
+        // dd(Article::where(function ($query) {
+        //     $query->where('is_published', true)
+        //         ->where('minutes_to_read', 7);
+        // })->get()); //Get all published articles with 7 minutes to read using closure
+
+        //whereStrict
+        // dd(Article::whereStrict('is_published', true)->get()); //Get all articles where is_published is true using strict comparison
+
+        //whereBetween
+        // dd(Article::whereBetween('minutes_to_read', [5, 10])->get()); //Get all articles where minutes_to_read is between 5 and 10
 
     }
 
