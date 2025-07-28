20e14b15ee0996fb1a314f140f17cb78954f506c
ShodanMaster <akshaykrishnan922@gmail.com>
2025-07-28 23:29:13 +0530

    Working With Numbers

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -11,7 +12,19 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        
+        //dd(Article::count()); //Count returns the number of articles
+        //dd(Article::where('is_published', true)->count()); //Count returns the number of published articles
+        // dd(Article::pluck('user_id')->countBy()); //group by perticular column and return the count of group
+        // dd(Article::max('minutes_to_read')); //Get the maximum value of a column
+        // dd(Article::whereBetween('user_id', [20, 30])->max('minutes_to_read')); //Get the maximum value of a column within a range
+        // dd(Article::min('minutes_to_read')); //Get the minimum value of a column
+        // dd(Article::pluck('minutes_to_read')->median()); //Get the median of a column
+        // dd(Article::pluck('minutes_to_read')->mode()); //Get the mode of a column
+        // dd(Article::inRandomOrder()->first()); //Get a random article
+        // dd(Article::inRandomOrder()->value('id')); //Get a random article id
+        // dd(Article::sum('minutes_to_read')); //Get the sum of a column
+
+
     }
 
     /**
